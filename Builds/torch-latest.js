7909007c93cc7a1958463e0299366a9241535009
roonilwazlib7 <roonilwazlib7@hotmail.com>
2016-11-29 19:47:46 -0700

    about to rewrite the canvas renderer

--- Builds/torch-latest.js
+++ Builds/torch-latest.js
@@ -5256,4 +5256,4 @@ if(!i(t)||0>t)throw new Error("k must be a non-negative integer");if(e&&e.isMatr
 
 }).call(this);
 
-Torch.version = '0.4.44'
+Torch.version = '0.4.45'
--- Core/version.js
+++ Core/version.js
@@ -1,1 +1,1 @@
-Torch.version = '0.4.44'
+Torch.version = '0.4.45'
